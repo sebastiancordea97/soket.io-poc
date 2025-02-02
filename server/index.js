f060445593085eb441b75b728873db1e771358b2
sebastian.cordea <sebastian.cordea@omniatech.io>
2025-02-02 15:06:00 +0200

    chore(init): add socket io to the server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const http = require('http');
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
-console.log("Server OK");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
